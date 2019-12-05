2c0519c9731148cea0aee163e1eef47ba75d9af5
root <you@example.com>
2019-12-05 17:42:59 -0300

    Updates

---
+++ precache-manifest.1ba266cf7764b01dc4302f5f5169074e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e203f764869e5d7552ad7758e33206e",
+    "url": "/pipefy-ui-clone/index.html"
+  },
+  {
+    "revision": "cf883e32974d2baca4ed",
+    "url": "/pipefy-ui-clone/static/js/2.551e7c09.chunk.js"
+  },
+  {
+    "revision": "56eab999c052255d1583",
+    "url": "/pipefy-ui-clone/static/js/main.074d276d.chunk.js"
+  },
+  {
+    "revision": "244b5346c46bc9592adc",
+    "url": "/pipefy-ui-clone/static/js/runtime-main.eaf7ae1c.js"
+  }
+]);
